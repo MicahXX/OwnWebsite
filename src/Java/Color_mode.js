b24c4f4e2d32f53e4fd7752cab721ff0105a67b7
Micah <martinstefanovic043@gmail.com>
2025-08-30 16:38:29 +0200

    V1.2.2 Changed name fixed bugs.

---
+++ src/Java/Color_mode.js
@@ -0,0 +1,26 @@
+let lightmode = localStorage.getItem('lightmode');
+const lightmodeButton = document.getElementById('lightmode');
+
+const enableLightmode = () => {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'active');
+}
+
+const disableLightmode = () => {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode', null);
+}
+
+if(lightmode === "active") enableLightmode();
+
+lightmodeButton.addEventListener("click", (e) => {
+    lightmode = localStorage.getItem('lightmode');
+    lightmode !== "active" ? enableLightmode() : disableLightmode();
+})
+
+const changeText = document.querySelector("#lightmode");
+
+changeText.addEventListener("click", function() {
+    if (lightmode === "active") changeText.textContent = "Dark mode";
+    if (lightmode !== "active") changeText.textContent = "Light mode";
+});
