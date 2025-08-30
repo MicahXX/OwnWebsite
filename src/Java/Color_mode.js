d582754390e6df8eaed6babb0a358467160e35fc
Micah <martinstefanovic043@gmail.com>
2025-08-31 00:41:47 +0200

    V1.2.5 fixed bugs. (dark-lightmode text update)

--- src/Java/Color_mode.js
+++ src/Java/Color_mode.js
@@ -4,24 +4,23 @@ const lightmodeButton = document.getElementById('lightmode');
 const enableLightmode = () => {
     document.body.classList.add('lightmode');
     localStorage.setItem('lightmode', 'active');
+    lightmodeButton.textContent = "Light mode";
 }
 
 const disableLightmode = () => {
     document.body.classList.remove('lightmode');
     localStorage.setItem('lightmode', null);
+    lightmodeButton.textContent = "Dark mode";
 }
 
-if(lightmode === "active") enableLightmode();
+if(lightmode === "active") {
+    enableLightmode();
+} else {
+    disableLightmode();
+}
 
 lightmodeButton.addEventListener("click", () => {
     lightmode = localStorage.getItem('lightmode');
     lightmode !== "active" ? enableLightmode() : disableLightmode();
-})
-
-const changeText = document.querySelector("#lightmode");
-
-changeText.addEventListener("click", function() {
-    lightmode = localStorage.getItem('lightmode');
-    if (lightmode === "active") changeText.textContent = "Dark mode";
-    if (lightmode !== "active") changeText.textContent = "Light mode";
 });
+
