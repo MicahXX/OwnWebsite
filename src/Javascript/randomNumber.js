7396afdfeb5c018b2484df48ed7c505b46519882
MicahXX <micahaim1@gmail.com>
2025-09-11 13:36:03 +0100

    v2.2 made the game way better added health still want to add some things

--- src/Javascript/randomNumber.js
+++ src/Javascript/randomNumber.js
@@ -7,12 +7,6 @@ const wrongSound = document.getElementById("wrongSound");
 document.getElementById("submitBtn").addEventListener("click", function() {
     let guess = parseInt(document.getElementById("number").value, 10);
 
-    if (userHealth === 0) {
-        document.getElementById("userHealth").innerHTML = "<h2>You lost all your lives);</h2>";
-    } else if (userHealth <= 0) {
-        document.getElementById("userHealth").innerHTML = "<h2>You have no lives left reload website for another try!</h2>";
-    }
-
     if (isNaN(guess) && userHealth > 0) {
         document.getElementById("result").innerHTML = "<h2>Hey, guess a number!</h2>";
         wrongSound.play();
@@ -25,11 +19,17 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     } else if (guess === secretNumber && userHealth > 0) {
         document.getElementById("result").innerHTML = "<h2>You guessed the correct number!!!</h2>";
         correctSound.play();
-    } else if (userHealth > 0) {
+    } else if (userHealth > 0 && userHealth !== 0) {
         userHealth--;
         document.getElementById("result").innerHTML = "<h2>You guessed the wrong number.....</h2>";
         document.getElementById("userHealth").innerHTML = userHealth;
         wrongSound.play();
+    }
+
+    if (userHealth === 0) {
+        document.getElementById("userHealth").innerHTML = "<h2>You lost all your lives...</h2>";
+    } else if (userHealth <= 0) {
+        document.getElementById("userHealth").innerHTML = "<h2>You have no lives left reload website for another try!</h2>";
     }
 });
 
