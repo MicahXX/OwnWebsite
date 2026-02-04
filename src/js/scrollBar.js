02d06df7fa60e3356c190b680e69cb9e8551c727
Marti <shirogg10@gmail.com>
2026-02-05 00:27:30 +0100

    remove scrollbar

--- src/js/scrollBar.js
+++ src/js/scrollBar.js
@@ -1,6 +1,18 @@
+let ticking = false;
+
 window.addEventListener('scroll', () => {
-    const scrolled = window.scrollY;
-    const height = document.documentElement.scrollHeight - window.innerHeight;
-    document.getElementById('scroll-bar').style.width =
-        `${(scrolled / height) * 100}%`;
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            const scrolled = window.scrollY;
+            const height = document.documentElement.scrollHeight - window.innerHeight;
+            const scrollBar = document.getElementById('scroll-bar');
+
+            if (scrollBar) {
+                scrollBar.style.width = `${(scrolled / height) * 100}%`;
+            }
+
+            ticking = false;
+        });
+        ticking = true;
+    }
 });
