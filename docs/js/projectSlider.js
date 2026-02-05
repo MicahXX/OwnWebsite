67478f276608f43f906af55ac7475681d681bfb1
Marti <shirogg10@gmail.com>
2026-02-05 16:47:46 +0100

    fix

--- docs/js/projectSlider.js
+++ docs/js/projectSlider.js
@@ -1,19 +1,12 @@
 const splide = new Splide('.splide', {
     type: 'loop',
     perPage: 1,
-    autoplay: true,
-    interval: 100000000,
-    pauseOnHover: true,
+    autoplay: false,
     arrows: true,
     pagination: true,
     speed: 500,
     resetProgress: false,
 }).mount();
-
-splide.on('move', () => {
-    splide.Components.Autoplay.pause();
-    setTimeout(() => splide.Components.Autoplay.play(), 1000);
-});
 
 let typed = '';
 const secret = 'micah';
